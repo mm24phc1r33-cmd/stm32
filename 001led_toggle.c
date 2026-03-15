4ba31955594cb1bb4dee18ff73b937697f829c5b
Monika <mm24phc1r33@student.nitw.ac.in>
2026-03-15 19:08:47 +0530

    Update 001led_toggle.c

--- 001led_toggle.c
+++ 001led_toggle.c
@@ -1,1 +1,31 @@
+#include "stm32f401xx_gpio_driver.h"
+
+void delay(void)
+{
+    for(uint32_t i = 0; i < 500000/2; i++);
+}
+
+int main(void)
+{
+    GPIO_Handle_t GpioLed;
+
+    /* Enable clock for GPIOA */
+    GPIO_PeriClockControl(GPIOA, ENABLE);
+
+    GpioLed.pGPIOx = GPIOA;
+    GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_5;
+    GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+    GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+    GpioLed.GPIO_PinConfig.GPIO_PinOpType = GPIO_OP_TYPE_PP;
+    GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+    /* Initialize GPIO */
+    GPIO_Init(&GpioLed);
+
+    while(1)
+    {
+        GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
+        delay();
+    }
+}
 
